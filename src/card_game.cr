ef1531d28fe8a97e41bcbb29e81d49fd630b4db4
Jason Landry <jasonl99@gmail.com>
2017-01-28 13:54:29 -0500

    wip

--- src/card_game.cr
+++ src/card_game.cr
@@ -20,6 +20,7 @@ module CardGame
     end
     game_name = context.params.url["game"]
     javascript, card_game = CardGame.preload(name: game_name, session_id: context.session.id, create: true)
+    javascript2, card_game2 = CardGame.preload(name: "jason", session_id: context.session.id, create: true)
     render "src/card_game/page.slang"
   end
 
--- src/card_game/card_game.cr
+++ src/card_game/card_game.cr
@@ -2,28 +2,38 @@ module CardGame
   class CardGame < Lattice::Connected::WebObject
     VALUES = %w(2 3 4 5 6 7 8 9 10 Jack Queen King Ace)
     SUITS  = %w(Hearts Diamonds Spades Clubs)
+    @version = 1
     property url : String?
-    property cards : Array(String) = new_deck
+    property deck : Array(String) = new_deck
+    property hand = [] of String
+    property version
 
     def content
       render "./src/card_game/card_game.slang"
     end
 
+    def initialize(@name)
+      (1..5).each {|c| hand << draw_card}
+      super
+    end
+
+    # action comes in the form of dom=>param
     def subscriber_action(action : Hash(String,JSON::Type), session_id : String)
       puts "action #{action} by session #{session_id} to #{self.class.to_s.colorize(:green).to_s} #{name.colorize(:green).to_s}"
-      if action.first_key == "click"
-        dom_id = action.first_value
-        return if dom_id == "hand"  # just a hack for the time being to avoid testing domid
-        player_name = Session.get(session_id).as(Session).string("name")  # we assume that this has been validated and a session exists and name is set
-        card = draw_card
-        subscribers.each &.send({"message"=>"#{player_name} drew card is #{card} for #{dom_id}"}.to_json)
-        subscribers.each &.send({"dom"=>{"action"=>"update", "id"=>dom_id, "value"=>card}}.to_json)
+      if action.first_value == "click"
+        clicked_id = action.first_key
+        if clicked_id.match(/card-[0-4]/) && (card = index_from( source: clicked_id, max: hand.size - 1))
+          player_name = Session.get(session_id).as(Session).string("name")  # we assume that this has been validated and a session exists and name is set
+          hand[card] = draw_card
+          update({"id"=>clicked_id, "value"=>hand[card]})
+          update({"id"=>"#{dom_id}-cards-remaining", "value"=>deck.size})
+        end
       end
     end
 
     def draw_card
-      card = cards.sample
-      cards.delete card
+      card = deck.sample
+      deck.delete card
       card  # OPTIMIZE does delete already return this?
     end
 
