48b2c366b104aa4adbfe9bdd328b079fa3ffd99e
Jason Landry <jasonl99@gmail.com>
2017-01-30 21:58:29 -0500

    added RingBuffer class for chat messages

--- src/card_game/chat_room.cr
+++ src/card_game/chat_room.cr
@@ -1,27 +1,31 @@
 require "./chat_message"
+require "./ring_buffer"
 module CardGame
   class ChatRoom < Lattice::Connected::WebObject
-    MAX_MESSAGES = 100
-    property messages = Array(ChatMessage | Nil).new(size: MAX_MESSAGES, value: nil)
-    property current_message = 0  # an index in the circular buffer for messages
+    MAX_MESSAGES = 5
+    # property messages = Array(ChatMessage | Nil).new(size: MAX_MESSAGES, value: nil)
+    # property current_message = 0  # an index in the circular buffer for messages
+    @messages = RingBuffer(ChatMessage).new(size: MAX_MESSAGES)
+    property messages
 
     def initialize(@name)
       super
     end
 
     def send(chat_message : ChatMessage)
+      messages << chat_message
       insert({"id"=>"#{dom_id}-message-holder", "value"=>chat_message.content})
     end
 
     # switch to using #update_attribute once it has been modified to update
     # specific sockets
     def subscribed(session_id : String, socket : HTTP::WebSocket)
-      session = Session.get(session_id)
-      user_name = session.as(Session).string?("name")
-      puts "Chatroom {self.name} subscribed by #{session_id}"
-      msg = {"action"=>"update_attribute", "id"=>"#{dom_id}-chatname", "attribute"=>"value", "value"=>user_name}
-      puts "attempting to update dom with"
-      socket.send({"dom"=>msg}.to_json)
+      # session = Session.get(session_id)
+      if (user_name = session_string(session_id: session_id, value_of: "name"))
+        puts "Chatroom {self.name} subscribed by #{session_id}"
+        personalize = {"action"=>"update_attribute", "id"=>"#{dom_id}-chatname", "attribute"=>"value", "value"=>user_name}
+        update_attribute(personalize, [socket])
+      end
     end
 
 
@@ -30,7 +34,7 @@ module CardGame
     end
 
     def rendered_messages
-      messages.reject(&.nil?).map(&.as(ChatMessage).content).join("\n")
+      messages.values.reject(&.nil?).map(&.as(ChatMessage).content).join("\n")
     end
 
 		def display_form
