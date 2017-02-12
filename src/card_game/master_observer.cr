48593ffbf6d4cf92bd72530fd307d19584925805
Jason Landry <jasonl99@gmail.com>
2017-02-12 01:25:42 -0500

    wip

--- src/card_game/master_observer.cr
+++ src/card_game/master_observer.cr
@@ -2,18 +2,13 @@ module CardGame
   #TODO this might be better as a class rather than an instance
   class MasterObserver < Lattice::Connected::WebObject
 
-    
-    @game_stats = Hash(String,String | Int32).new
+    @game_stats = Hash(String, String | Int32 ).new
 
     def game_stats
       @game_stats
     end
 
-    def content
-      render "src/card_game/master_observer.slang"
-    end
-
-    def on_event(event, sender)
+    def load_stats
       total_subs = 0
       empty_games = 0
       @game_stats["Games"] = CardGame.instances.size
@@ -26,7 +21,19 @@ module CardGame
       @game_stats["Empty Games"] = empty_games
       @game_stats["Total Subs"] = total_subs
       @game_stats["Total Events"] = @game_stats.fetch("Total Events",0).as(Int32) + 1
-      update({"id"=>dom_id, "value"=>content})
+    end
+
+    def content
+      load_stats
+      render "src/card_game/master_observer.slang"
+    end
+
+    def on_event(event, sender)
+      load_stats
+      @game_stats.each do |(k,v)|
+        update_component k.gsub(" ","-").downcase, v
+      end
+      #update({"id"=>dom_id, "value"=>content})
     end
   end
 end
