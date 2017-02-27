89d7c14474dc11470785bcb25b564c3273a1bf82
Jason Landry <jasonl99@gmail.com>
2017-02-27 16:28:33 -0500

    wip

--- src/card_game.cr
+++ src/card_game.cr
@@ -18,12 +18,6 @@ puts "kemal-session config finished"
 
 module CardGame
 
-  @@global_stats = GlobalStats.new(name: "global_stats")
-
-  def self.global_stats
-    @@global_stats
-  end
-
   @@master_observer = MasterObserver.new name: "MasterObserver"
   class_getter master_observer
 
@@ -39,23 +33,11 @@ module CardGame
 
   Lattice::Core::Application.route_socket(user_class: Player)
 
-
-  # get "/stats" do |context|
-  #   javascript, global_stats = GlobalStats.preload(name: "global_stats", session_id: context.session.id, create: true)
-  #   global_stats = GlobalStats.find_or_create("global_stats")
-  #   javascript = global_stats.javascript
-  #   if global_stats
-  #     global_stats.update_stats
-  #     render "src/card_game/stats.slang"
-  #   end
-  # end
-
   get "/cardgame/:game" do |context|
     player = Player.find_or_create(context.session.id)
     user_name = player.name
     game_name = context.params.url["game"]
     begin
-#      javascript, card_game = CardGame.preload(name: game_name, session_id: context.session.id, create: true)
       card_game = CardGame.find_or_create(game_name).as(CardGame)
       games = [card_game].compact
       render "src/card_game/games.slang"
